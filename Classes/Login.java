744f9aa6c8342bd1df43ac206ae1cd2259ca1f98
SOUVIK SAHA <souvik21102001@gmail.com>
2025-07-30 13:31:51 +0530

    Login class updated

--- Classes/Login.java
+++ Classes/Login.java
@@ -2,87 +2,109 @@ package Classes;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
 
-public class Login extends JFrame {
-
+public class Login extends JFrame implements ActionListener {
     JTextField textField;
     JPasswordField passwordField;
-    JButton btn1, btn2;
+    JButton btnLogin, btnCancel, btnSignup;
 
     Login() {
         setTitle("Arogya Healthcare Login");
-
-        // --- Set solid teal background ---
         getContentPane().setBackground(new Color(23, 95, 127));
         setLayout(null);
 
-        // --- Heading Label ---
         JLabel heading = new JLabel("Arogya Healthcare");
         heading.setBounds(60, 10, 300, 30);
         heading.setFont(new Font("SansSerif", Font.BOLD, 22));
         heading.setForeground(Color.WHITE);
         add(heading);
 
-        // --- Username Label ---
         JLabel name = new JLabel("Username:");
         name.setBounds(60, 60, 120, 30);
         name.setFont(new Font("SansSerif", Font.BOLD, 16));
         name.setForeground(Color.WHITE);
         add(name);
 
-        // --- Password Label ---
         JLabel password = new JLabel("Password:");
         password.setBounds(60, 110, 120, 30);
         password.setFont(new Font("SansSerif", Font.BOLD, 16));
         password.setForeground(Color.WHITE);
         add(password);
 
-        // --- Username Field ---
         textField = new JTextField();
         textField.setBounds(180, 60, 200, 30);
         textField.setFont(new Font("SansSerif", Font.PLAIN, 16));
-        textField.setForeground(Color.BLACK);
-        textField.setBackground(Color.WHITE);
         add(textField);
 
-        // --- Password Field ---
         passwordField = new JPasswordField();
         passwordField.setBounds(180, 110, 200, 30);
         passwordField.setFont(new Font("SansSerif", Font.PLAIN, 16));
-        passwordField.setForeground(Color.BLACK);
-        passwordField.setBackground(Color.WHITE); 
         add(passwordField);
 
-        // --- Logo Image ---
         ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("Icons/login.png"));
         Image image1 = icon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
         JLabel imageLabel = new JLabel(new ImageIcon(image1));
         imageLabel.setBounds(420, 60, 150, 150);
         add(imageLabel);
 
-        // --- Login Button ---
-        btn1 = new JButton("Login");
-        btn1.setBounds(60, 170, 150, 40);
-        btn1.setFont(new Font("SansSerif", Font.BOLD, 16));
-        btn1.setForeground(Color.WHITE);
-        btn1.setBackground(new Color(0, 150, 136)); // Dark Teal
-        btn1.setFocusPainted(false);
-        add(btn1);
+        btnLogin = new JButton("Login");
+        btnLogin.setBounds(60, 170, 100, 40);
+        btnLogin.setFont(new Font("SansSerif", Font.BOLD, 14));
+        btnLogin.setBackground(new Color(0, 150, 136));
+        btnLogin.setForeground(Color.WHITE);
+        btnLogin.addActionListener(this);
+        add(btnLogin);
 
-        // --- Cancel Button ---
-        btn2 = new JButton("Cancel");
-        btn2.setBounds(230, 170, 150, 40);
-        btn2.setFont(new Font("SansSerif", Font.BOLD, 16));
-        btn2.setForeground(Color.WHITE);
-        btn2.setBackground(new Color(220, 53, 69)); // Soft Red
-        btn2.setFocusPainted(false);
-        add(btn2);
+        btnCancel = new JButton("Cancel");
+        btnCancel.setBounds(180, 170, 100, 40);
+        btnCancel.setFont(new Font("SansSerif", Font.BOLD, 14));
+        btnCancel.setBackground(new Color(220, 53, 69));
+        btnCancel.setForeground(Color.WHITE);
+        btnCancel.addActionListener(this);
+        add(btnCancel);
 
-        // --- Frame Settings ---
+        btnSignup = new JButton("Sign Up");
+        btnSignup.setBounds(300, 170, 100, 40);
+        btnSignup.setFont(new Font("SansSerif", Font.BOLD, 14));
+        btnSignup.setBackground(new Color(70, 130, 180));
+        btnSignup.setForeground(Color.WHITE);
+        btnSignup.addActionListener(this);
+        add(btnSignup);
+
         setSize(650, 280);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnLogin) {
+            try {
+                ConnectSql connectSql = new ConnectSql();
+                String username = textField.getText();
+                String password = String.valueOf(passwordField.getPassword());
+
+                String query = "SELECT * FROM login WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "'";
+                ResultSet resultSet = connectSql.statement.executeQuery(query);
+
+                if (resultSet.next()) {
+                    new Reception();
+                    this.setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else if (e.getSource() == btnCancel) {
+            System.exit(0);
+        } else if (e.getSource() == btnSignup) {
+            new Signup();
+            this.setVisible(false);
+        }
     }
 
     public static void main(String[] args) {
